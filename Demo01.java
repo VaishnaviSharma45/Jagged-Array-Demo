4fb84327513eb93c09238fa53003c0b2bd97736d
VaishnaviSharma45 <165872229+VaishnaviSharma45@users.noreply.github.com>
2024-04-03 11:38:47 +0530

    Add files via upload

---
+++ Demo01.java
@@ -0,0 +1,12 @@
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+int[][] arr= {{1,2,3},{4,5,6,7,8},{5,9,6,8,7,3}};
+System.out.println("No of rows = "+arr.length);
+System.out.println("No of columns in 1st row = "+arr[0].length);
+System.out.println("No of columns in 2nd row = "+arr[1].length);
+System.out.println("No of columns in 3rd row = "+arr[2].length);
+	}
+
+}
